6c18a01c2ad8526d39a3f04d7a25e159eacb6297
Thomas Roehr <thomas.roehr@dfki.de>
2020-01-07 12:13:58 +0100

    Do not use the rock_standard_layout()

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-#include <no_standard_layout_package/Dummy.hpp>
 
 int main(int argc, char** argv)
 {
-    no_standard_layout_package::DummyClass dummyClass;
-    dummyClass.welcome();
-
     return 0;
 }
